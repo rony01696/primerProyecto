99054c56585e68e5292129a0e61da1f149513bcf
rony01696 <50722615+rony01696@users.noreply.github.com>
2019-08-21 18:04:54 -0500

    Update main.cpp

--- asdasd/main.cpp
+++ asdasd/main.cpp
@@ -4,6 +4,21 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int a;
+    int b;
+    int c;
+    
+    cout<<"Ingrese a: ";
+    cin>>a;
+    cout<<"Ingrese b: ";
+    cin>>b;
+    cout<<"Ingrese c: ";
+    cin>>c;
+    if (a>b && a>c)
+        cout<<"a es mayor"<<endl;
+    if (b>a && b>c)
+        cout<<"b es mayor"<<endl;
+    if (c>a && c>b)
+    cout<<"c es mayor"<<endl;
     return 0;
 }
