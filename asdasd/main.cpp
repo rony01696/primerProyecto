c33571184dc3fff54aa4f1d3202ac24a9a39e271
rony01696 <50722615+rony01696@users.noreply.github.com>
2019-08-23 09:25:58 -0500

    Update main.cpp

--- asdasd/main.cpp
+++ asdasd/main.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
-
 using namespace std;
+int main() {
 
-int main()
-{
     int a;
     int b;
     int c;
     
-    cout<<"Ingrese a: ";
+    cout<<"INGRESE EL VALOR DE A: ";
     cin>>a;
-    cout<<"Ingrese b: ";
+    cout<<"INGRESE EL VALOR DE B: ";
     cin>>b;
-    cout<<"Ingrese c: ";
+    cout<<"INGRESE EL VALOR DE C: ";
     cin>>c;
-    if (a>b && a>c)
-        cout<<"a es mayor"<<endl;
-    if (b>a && b>c)
-        cout<<"b es mayor"<<endl;
-    if (c>a && c>b)
-    cout<<"c es mayor"<<endl;
-    return 0;
+
+    if(a>b && a>c)
+        cout<<"A ES EL MAYOR";
+    else
+        if(b>c)
+            cout<<"B ES EL MAYOR";
+        else
+            cout<<"C ES EL MAYOR";
+
+  return 0;
 }
+
