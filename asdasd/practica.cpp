0cc00f31d95a0b569b4cbc820ab4a86dbe4a0d25
rony01696 <50722615+rony01696@users.noreply.github.com>
2019-09-19 23:43:10 -0500

    Add files via upload

---
+++ asdasd/practica.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+
+/*
+//EJERCICIO 1
+int main() {
+	int a;
+	cin >> a;
+	
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	long e = 0;
+
+	for (int i = 1; i <= a; i++) {
+		
+		if (i % 3 == 0) {
+			b = b + i;
+		}
+		if (i % 5 == 0) {
+
+			c = c + i;
+		}
+
+		if ((i % 3 == 0) && (i % 5 == 0)) {
+			d = d + i;
+		
+		}
+		
+		
+
+	}
+	e = b + c - d;
+	cout << e << endl;
+
+
+	return 0;
+	
+}
+*/
+
+
+
+//EJERCICIO 2
+
+int inverso(){
+
+	int a;
+	int b;
+	int c=0;
+	cin >> a;
+	while (a>0) {
+		b = a % 10;
+		a = a / 10;
+		c = c * 10 + b;
+	}
+
+	return c;
+	cout << c << endl;
+}
+
+
