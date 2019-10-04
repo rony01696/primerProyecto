477bd10aee67e2ba9a69554374fee663ae694167
rony01696 <50722615+rony01696@users.noreply.github.com>
2019-10-04 11:12:12 -0500

    Update punteros.cpp

--- asdasd/punteros.cpp
+++ asdasd/punteros.cpp
@@ -1,27 +1,20 @@
 #include <iostream>
 
 using namespace std;
-/*
+
 void swap(int *arr, int a, int b) {
-	int c = *arr;
-	*(arr+a) = *(arr+(b-a));
-	*(arr+(b-a)) = c;
-
+	int c = *(arr+a);
+	*(arr+a) = *(arr+b);
+	*(arr+b) = c;
 }
-
-
-
 
 void imprimir(int arr[], int tam) {
     cout << "[ ";
 	while(tam--){
-       cout << "}"  cout << *arr++ << " ";
+       cout << *arr++ << " ";
 	}
 	cout << "]" << endl;
 }
-
-
-
 int suma(int *arr, int tam) {
 	int suma = 0;
 	for (int i = 0; i < tam; i++)
@@ -30,43 +23,99 @@ int suma(int *arr, int tam) {
 }
 
 
-*/
 int suma_re(int *arr, int tam) {
 	if (tam == 1)
-		return *(arr+0);
+		return *arr++;
 	return *(arr+(tam - 1)) + suma_re(arr, tam - 1);
 }
 
 
-/*
-int suma_re(int arr[], int tam) {
-	if (tam == 1)
-		return arr[0];
-	return arr[tam - 1] + suma_re(arr, tam - 1);
-}
 void inversa_re(int *arr, int tam, int i=0) {
-	if (tam-1 > i) {
+	tam--;
+	if (tam > i) {
 		swap(arr, i, tam);
-		inversa_re(arr, tam-1, i+1);
+		inversa_re(arr, tam, i+1);
 	}
 }
 
 
+void inversa(int *arr, int tam) {
 
-
-
-
-void inversa(int arr[], int tam) {
-	int a;
 	int b = tam / 2;
+    	tam=tam-1;
 	for (int i = 0; i < b; i++) {
 		swap(arr, i, tam);
-		tam = tam - 1;
+		tam--;
+	}
+}
+
+void quick(int *arr,int primero, int ultimo){
+   	int i=primero;
+   	int j=ultimo;
+    	int pivote=(*(arr+i)+*(arr+j))/2;
+    	while (i<j){
+        	while(*(arr+i)<pivote){
+            		i++;
+        	}
+        	while(*(arr+j)>pivote){
+            		j--;
+        	}
+        	if (i<=j){
+            		swap(arr,i,j);
+            		i++;
+            		j--;
+        	}
+   	 }
+    	if (j>primero)
+       	 quick(arr,primero,j);
+    	
+    	if (i<ultimo)
+      	  quick(arr,i,ultimo);
+    
+}
+
+
+
+
+void burbuja(int *arr,int tam){
+    	bool cambio=true;
+    	while (cambio){
+        	cambio=false;
+        	for(int i=0;i<tam-1;i++){
+           		 if (*(arr+i)>*(arr+i+1)){
+                	swap(arr,i,i+1);
+                	cambio=true;
+            	}
+        }
+        tam--;
+    }
+}
+
+
+
+
+
+void inserccion(int *arr, int tam){
+	for (int i = 0; i < tam; i++) {
+		int guardar = *(arr+i);
+		int j = i;
+		while (j > 0 && *(arr+(j-1)) > guardar) {
+			*(arr+j) = *(arr+(j-1));
+			j -= 1;
+		}
+		*(arr+j) = guardar;
 	}
 }
 
 
-*/
+
+
+
+
+
+
+
+
 int main() {
 	int x[4];
 	for (int i = 0; i <4; i++)
